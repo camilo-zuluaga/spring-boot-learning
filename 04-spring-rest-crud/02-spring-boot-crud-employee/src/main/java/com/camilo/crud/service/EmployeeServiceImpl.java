b51514d5834ce379b6c7fec146b7bee1892791d3
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-10-22 15:52:14 -0500

    feat: service implement new methods DAO and handle transactions

--- 04-spring-rest-crud/02-spring-boot-crud-employee/src/main/java/com/camilo/crud/service/EmployeeServiceImpl.java
+++ 04-spring-rest-crud/02-spring-boot-crud-employee/src/main/java/com/camilo/crud/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.camilo.crud.service;
 
 import com.camilo.crud.dao.EmployeeDAO;
 import com.camilo.crud.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,14 +12,33 @@ import java.util.List;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeDAO employeeDAO;
+    private final EmployeeDAO employeeDAO;
 
     public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
         this.employeeDAO = employeeDAO;
     }
 
+    // It is good practice to do Transactional handling on the service layer
+
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int employeeId) {
+        return employeeDAO.findById(employeeId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employeeToSave) {
+        return employeeDAO.save(employeeToSave);
+    }
+
+    @Transactional
+    @Override
+    public void delete(int employeeId) {
+        employeeDAO.delete(employeeId);
     }
 }
